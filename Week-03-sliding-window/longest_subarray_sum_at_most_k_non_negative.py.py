e96455e6166983efb225d8d59da9fbf92d3c3e42
rehuuu01 <mohammedrehan8904@gmail.com>
2026-01-22 22:49:46 +0530

    modified non_negative.py

--- Week-03-sliding-window/longest_subarray_sum_at_most_k_non_negative.py.py
+++ Week-03-sliding-window/longest_subarray_sum_at_most_k_non_negative.py.py
@@ -1,3 +1,5 @@
+# Works ONLY for non-negative numbers
+
 def longest_subarray_sum_at_most_k(arr, k):
     n = len(arr)
     max_length = 0
@@ -7,16 +9,18 @@ def longest_subarray_sum_at_most_k(arr, k):
     for right in range(n):
         current_sum += arr[right]
 
-        while current_sum > k and left <= right:
+        while current_sum > k:
             current_sum -= arr[left]
             left += 1
 
         max_length = max(max_length, right - left + 1)
 
     return max_length
+
+
 if __name__ == "__main__":
     arr = list(map(int, input("Enter elements: ").split()))
     k = int(input("Enter the sum limit (k): "))
-    # Call the function and print the result
+
     result = longest_subarray_sum_at_most_k(arr, k)
     print(f"The length of the longest subarray with sum at most {k} is: {result}")
